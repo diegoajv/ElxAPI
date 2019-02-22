3c3a10be80744ff1635c4df4bd4afb46c4261d7d
diegoajv <diegojosevilla@outlook.com>
2019-02-22 16:12:56 -0600

    Enhancement[api]: Add projects and documents resources to router

--- rest-api/lib/rest_api_web/router.ex
+++ rest-api/lib/rest_api_web/router.ex
@@ -5,7 +5,20 @@ defmodule RestApiWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :json_api do
+    plug :accepts, ["json-api"]
+    plug JaSerializer.Deserializer
+  end
+
   scope "/api", RestApiWeb do
     pipe_through :api
+
+    resources "/documents", DocumentController, only: [:index, :show]
+  end
+
+  scope "/api", RestApiWeb do
+    pipe_through :json_api
+
+    resources "/projects", ProjectController, only: [:index, :show]
   end
 end
