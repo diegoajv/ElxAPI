1351896b5f94d911fdea14a4ca0574e52dcb8b40
diegoajv <diegojosevilla@outlook.com>
2019-02-27 17:05:31 -0600

    Fix[seeds]: Change Posts seeds

--- graphql-api/priv/repo/seeds/posts_seeds.exs
+++ graphql-api/priv/repo/seeds/posts_seeds.exs
@@ -1,10 +1,57 @@
 alias GraphqlApi.Repo
 alias GraphqlApi.Blog.Post
 
-for _ <- 1..10 do
-  Repo.insert!(%Post{
-    title: Faker.Lorem.sentence,
-    body: Faker.Lorem.paragraph,
-    user_id: [1, 2] |> Enum.take_random(1) |> hd
-  })
-end
+Repo.insert!(%Post{
+  title: Faker.Lorem.sentence,
+  body: Faker.Lorem.paragraph,
+  data: %{
+    "site" => "ABC",
+    "section" => "Sports",
+    "topic" => "Soccer"
+  },
+  user_id: [1, 2] |> Enum.take_random(1) |> hd
+})
+
+Repo.insert!(%Post{
+  title: Faker.Lorem.sentence,
+  body: Faker.Lorem.paragraph,
+  data: %{
+    "site" => "DEF",
+    "section" => "Politics",
+    "topic" => "National"
+  },
+  user_id: [1, 2] |> Enum.take_random(1) |> hd
+})
+
+Repo.insert!(%Post{
+  title: Faker.Lorem.sentence,
+  body: Faker.Lorem.paragraph,
+  data: %{
+    "site" => "ABC",
+    "section" => "Videogames",
+    "topic" => "Xbox"
+  },
+  user_id: [1, 2] |> Enum.take_random(1) |> hd
+})
+
+Repo.insert!(%Post{
+  title: Faker.Lorem.sentence,
+  body: Faker.Lorem.paragraph,
+  data: %{
+    "site" => "DEF",
+    "section" => "Economics",
+    "topic" => "Asia"
+  },
+  user_id: [1, 2] |> Enum.take_random(1) |> hd
+})
+
+Repo.insert!(%Post{
+  title: Faker.Lorem.sentence,
+  body: Faker.Lorem.paragraph,
+  data: %{
+    "site" => "ABC",
+    "section" => "Videogames",
+    "topic" => "PS3"
+  },
+  user_id: [1, 2] |> Enum.take_random(1) |> hd
+})
