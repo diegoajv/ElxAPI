f0a768e8f2ac02032cb879572ce85617e0f9eb6d
diegoajv <diegojosevilla@outlook.com>
2019-02-27 17:05:14 -0600

    Feat[post-data]: Add type and resolver for Post

--- graphql-api/lib/graphql_api_web/resolvers/post_resolver.ex
+++ graphql-api/lib/graphql_api_web/resolvers/post_resolver.ex
@@ -2,6 +2,13 @@ defmodule GraphqlApiWeb.Resolvers.PostResolver do
   alias GraphqlApi.Blog
 
   def all(_args, _info) do
+    IO.inspect(Blog.list_posts())
     {:ok, Blog.list_posts()}
+  end
+
+  def key(key_name) do
+    fn parent, _, _ ->
+      {:ok, Map.get(parent, key_name)}
+    end
   end
 end
--- graphql-api/lib/graphql_api_web/schemas/types.ex
+++ graphql-api/lib/graphql_api_web/schemas/types.ex
@@ -1,6 +1,7 @@
 defmodule GraphqlApiWeb.Schemas.Types do
   use Absinthe.Schema.Notation
   use Absinthe.Ecto, repo: GraphqlApi.Repo
+  alias GraphqlApiWeb.Resolvers
 
   object :user do
     field :id, :id
@@ -9,10 +10,17 @@ defmodule GraphqlApiWeb.Schemas.Types do
     field :posts, list_of(:post), resolve: assoc(:posts)
   end
 
+  object :attribute do
+    field :site, :string, resolve: Resolvers.PostResolver.key("site")
+    field :section, :string, resolve: Resolvers.PostResolver.key("section")
+    field :topic, :string, resolve: Resolvers.PostResolver.key("topic")
+  end
+
   object :post do
     field :id, :id
     field :title, :string
     field :body, :string
+    field :data, :attribute
     field :user, :user, resolve: assoc(:user)
   end
 end
