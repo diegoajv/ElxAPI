dc1f4f3d7cbed08f51ef55d37cb7acfc7082b64d
diegoajv <diegojosevilla@outlook.com>
2019-02-27 15:23:51 -0600

    Feat[user]: Add support to query User by ID

--- graphql-api/lib/graphql_api_web/resolvers/user_resolver.ex
+++ graphql-api/lib/graphql_api_web/resolvers/user_resolver.ex
@@ -4,4 +4,13 @@ defmodule GraphqlApiWeb.Resolvers.UserResolver do
   def all(_args, _info) do
     {:ok, Admin.list_users()}
   end
+
+  def find_user(_args, %{id: id}, _resolution) do
+    case Admin.get_user!(id) do
+      nil ->
+        {:error, "User ID #{id} not found"}
+      user ->
+        {:ok, user}
+    end
+  end
 end
--- graphql-api/lib/graphql_api_web/schemas/schema.ex
+++ graphql-api/lib/graphql_api_web/schemas/schema.ex
@@ -4,12 +4,18 @@ defmodule GraphqlApiWeb.Schemas.Schema do
   import_types GraphqlApiWeb.Schemas.Types
 
   query do
+
     field :posts, list_of(:post) do
       resolve &Resolvers.PostResolver.all/2
     end
 
     field :users, list_of(:user) do
       resolve &Resolvers.UserResolver.all/2
+    end
+
+    field :user, :user do
+      arg :id, non_null(:id)
+      resolve &Resolvers.UserResolver.find_user/3
     end
   end
 end
